1d90a4cb83efdffa20948bc815581468be96157e
charlieomg <143298651+charlieomg@users.noreply.github.com>
2025-12-24 01:08:45 -0700

    Implement draggable GUI and update scroll text
    Added draggable GUI with an image button and updated scroll text.

--- realfebypass.lua
+++ realfebypass.lua
@@ -4190,7 +4190,7 @@ addCommand("reanim",function()
 end, {"reanimate"}, 3)
 
 --[[Add scrolling command list]]--
-local scrollText = "QuirkyCMD made by Cynatica | the best Admin abuse Utility! | "
+local scrollText = "doug fe bypass xd"
 if not isMobile then
 	scrollText ..= "press ; to open/hide the Ui | "
 end
@@ -4457,4 +4457,53 @@ if not inDatabase then
 
 end
 
-debugPrint("QuirkyCMD loaded successfully!")
+debugPrint("sigmar!")
+
+-- Xeno | Draggable GUI using DECAL thumbnail (works when decal won't render)
+
+local UIS = game:GetService("UserInputService")
+
+local parentGui = gethui and gethui() or game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+
+local gui = Instance.new("ScreenGui")
+gui.Name = "febypassi"
+gui.ResetOnSpawn = false
+gui.Parent = parentGui
+
+local img = Instance.new("ImageButton")
+img.Size = UDim2.new(0, 150, 0, 150)
+img.Position = UDim2.new(0, 10, 1, -160) -- bottom left
+img.BackgroundTransparency = 1
+img.Active = true
+
+img.Image = "rbxthumb://type=Asset&id=59414130&w=420&h=420"
+
+img.Parent = gui
+
+
+local dragging = false
+local dragOffset
+
+img.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		dragging = true
+		dragOffset = input.Position - img.AbsolutePosition
+	end
+end)
+
+img.InputEnded:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		dragging = false
+	end
+end)
+
+UIS.InputChanged:Connect(function(input)
+	if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+		img.Position = UDim2.fromOffset(
+			input.Position.X - dragOffset.X,
+			input.Position.Y - dragOffset.Y
+		)
+	end
+end)
+
