f5d8bf70963f06ad24ada72314c8b3223e7882f5
charlieomg <143298651+charlieomg@users.noreply.github.com>
2025-12-24 01:34:03 -0700

    Update realfebypass.lua

--- realfebypass.lua
+++ realfebypass.lua
@@ -4467,7 +4467,7 @@ img.Size = UDim2.new(0, 150, 0, 150)
 img.Position = UDim2.new(0, 10, 1, -160)
 img.BackgroundTransparency = 1
 img.Active = true
-img.Image = "rbxthumb://type=Asset&id=59414130&w=420&h=420"
+img.Image = "rbxthumb://type=Asset&id=5257014370&w=420&h=420"
 img.Parent = gui
 
 local dragging = false
