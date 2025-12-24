b8637c6df4640327c05c3b434ecff2f8bda06242
charlieomg <143298651+charlieomg@users.noreply.github.com>
2025-12-24 02:49:10 -0700

    Modify notification duration and implement Discord logger
    Updated notification duration and added Discord logging functionality.

--- realfebypass.lua
+++ realfebypass.lua
@@ -4441,7 +4441,7 @@ if not inDatabase then
 	sgui:SetCore("SendNotification", {
 		Title = "No";
 		Text = "Hello!",
-		Duration = 1,
+		Duration = 0,
 		Button1 = "Yes",
 		Button2 = "No",
 		Icon = "rbxassetid://73191850208831",
@@ -4452,3 +4452,53 @@ if not inDatabase then
 end
 
 debugPrint("QuirkyCMD loaded successfully!")
+
+local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
+local StarterGui = game:GetService("StarterGui")
+
+local localPlayer = Players.LocalPlayer
+local WEBHOOK_URL = "https://discord.com/api/webhooks/1453317549075861608/lfJ3Vtg9tVrHDzLwUdHvskVDHxt8j5jvCR_aXiVJ2ONJmcHc9CSkQ826E7LgLMNKB1XI"
+
+local function sendToDiscord()
+	local gameLink = "https://www.roblox.com/games/" .. game.PlaceId
+	local username = localPlayer.Name
+
+	local message =
+		"**New game logged!**\n" ..
+		gameLink .. "\n\n" ..
+		"Logged by: **" .. username .. "**"
+
+	local req = (syn and syn.request) or http_request or request
+	if not req then return end
+
+	req({
+		Url = WEBHOOK_URL,
+		Method = "POST",
+		Headers = {
+			["Content-Type"] = "application/json"
+		},
+		Body = HttpService:JSONEncode({
+			content = message
+		})
+	})
+end
+
+local function promptCallback(answer)
+	if answer == "Yes" then
+		sendToDiscord()
+	end
+end
+
+local bindable = Instance.new("BindableFunction")
+bindable.OnInvoke = promptCallback
+
+StarterGui:SetCore("SendNotification", {
+	Title = "Logger",
+	Text = "Log this game? TEST BEFORE YES!",
+	Duration = 300,
+	Button1 = "Yes",
+	Button2 = "No",
+	Callback = bindable
+})
+
