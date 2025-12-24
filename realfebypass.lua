f94d8788047cc84f57c62733eec78fd3378ca1c6
charlieomg <143298651+charlieomg@users.noreply.github.com>
2025-12-24 01:37:35 -0700

    Change image asset ID in realfebypass.lua

--- realfebypass.lua
+++ realfebypass.lua
@@ -4467,7 +4467,7 @@ img.Size = UDim2.new(0, 150, 0, 150)
 img.Position = UDim2.new(0, 10, 1, -160)
 img.BackgroundTransparency = 1
 img.Active = true
-img.Image = "rbxthumb://type=Asset&id=525701437&w=420&h=420"
+img.Image = "rbxthumb://type=Asset&id=160805099&w=420&h=420"
 img.Parent = gui
 
 local dragging = false
